c1dda2ed156d3cdae7c1b948229140856194fc4c
Fate <119829254+Master0fFate@users.noreply.github.com>
2023-05-22 14:48:09 +0200

    Update bot_en.py
    Complete re-write...
    Modernization to "/" (slash) commands.

--- bot_en.py
+++ bot_en.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import requests
 import json
@@ -16,30 +17,37 @@ def get_token():
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-@bot.command()
-async def wiki(ctx, *, search):
+
+@bot.tree.command(name="wiki") 
+@app_commands.describe(arg = "What to search")
+async def wikien(interaction: discord.Interaction, arg: str):
     """Search Wikipedia and return the summary of the first matching article"""
-    wikipedia_url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{search}"
+    wikipedia_url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{arg}"
     response = requests.get(wikipedia_url)
     data = response.json()
     summary = data.get("extract")
     if summary:
-        embed = discord.Embed(title=f"Search result of: {search}", description=summary, color=0xffffff)
-        await ctx.reply(embed=embed)
+        embed = discord.Embed(title=f"Search result: {arg}", description=summary, color=0xe91e63)
+        await interaction.response.send_message(embed=embed)
     else:
-        embed2 = discord.Embed(title=f"Error", description="That wikipedia page does not exist.", color=0xffffff)
-        await ctx.reply(embed=embed2)
+        embed2 = discord.Embed(title=f"Error", description="That wikipedia article does not exist.", color=0xe91e63)
+        await interaction.response.send_message(embed=embed2, ephemeral=True)
     
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.reply("Incorrect syntax. Use '/wiki [search]'")
+        await ctx.reply("Incorrect command usage. Use /help for help.")
 
 @bot.event
 async def on_ready():
     activity = discord.Activity(name='/wiki', type=discord.ActivityType.listening)
     await bot.change_presence(activity=activity)
-    print("WikiBot by github.com/Master0fFate\n")
+    print("WikiBot\n")
     print("Bot is ready...")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
 bot.run(get_token())
